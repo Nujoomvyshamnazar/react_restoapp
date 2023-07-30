748ad03213fe11bcfd89fe1dd9ee59406b14c188
Nujoomvyshamnazar <nujoomvyshamnazar@gmail.com>
2023-07-30 11:56:33 +0530

    added router and split page and material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom';
 import './App.css';
-
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+<Routes>
+<Route path="/" element={<Home/>} exact />
+<Route path="/about" element={<About/>} exact />
+<Route path="/menu" element={<Menu/>}  exact/>
+<Route path="/contact" element={<Contact/>} exact />
+</Routes>
+</Router>
+    
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+const Home = () =>{
+  return(
+    <div>
+      <h1>Home</h1>
+      <Button variant="contained">Hello World</Button>
+  
+    </div>
+  )
+}
+
+export default Home;
